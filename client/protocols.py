d2c147134c0bedd06f8c7d35011b872c847d2cae
jupiter2653 <jupiter653.jupiter653@gmail.com>
2020-04-13 15:34:18 +0200

    Comment

--- client/protocols.py
+++ client/protocols.py
@@ -5,14 +5,14 @@ sys.path.append("..")
 
 from FRP import FRPConect,FRPSend,FRPRcv
 
-def DNS(url,token):
+def DNS(url,token): #Protocole pour dns : envoie d'une requete avec l'ul+token et reception de l'ip+repertoire
     s = FRPConect("localhost",23486)
     FRPSend(s,{'url' : url.encode(),'token': token})
     rcv = FRPRcv(s)
     s.close()
     return rcv
         
-def get(url,directory,token):
+def get(url,directory,token): 
     s = FRPConect(url,12801)
     FRPSend(s,{'directory' : directory, 'token': token})
     rcv = FRPRcv(s)
